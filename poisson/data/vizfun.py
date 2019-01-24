ea6cc3ee1c8f1c00713b9e63ccb2dd09af48bd91
Peter <petervistar@hotmail.com>
2019-01-24 10:18:37 +0100

    Added poisson plots

--- poisson/data/vizfun.py
+++ poisson/data/vizfun.py
@@ -13,8 +13,6 @@ for files, names in zip(['statfun_poisson_CPU.dat','statfun_poisson_gpu1.dat','s
 						['statfun_poisson_CPU','statfun_poisson_gpu1','statfun_poisson_gpu2','statfun_poisson_gpu3']):
 	df1 = pd.read_csv(files,delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
 
-	df1 = df1.loc[df1['Threads'] < 16]
-
 	plt.figure()
 	df1.set_index("Memory", inplace=True)
 	ax = df1["MFlops"].plot(legend=True, style ='*-')
@@ -25,7 +23,8 @@ for files, names in zip(['statfun_poisson_CPU.dat','statfun_poisson_gpu1.dat','s
 	plt.xscale('log',basex=4)
 	#plt.yscale('log',basey=2)
 	ax = plt.gca().xaxis 
-	ax.set_major_formatter(ScalarFormatter()) 
+	if "gpu3" not in files:
+		ax.set_major_formatter(ScalarFormatter()) 
 	#ax = plt.gca().yaxis 
 	#ax.set_major_formatter(ScalarFormatter()) 
 	plt.savefig('MFlops_%s.png' % names, bbox_inches='tight')
@@ -40,7 +39,8 @@ for files, names in zip(['statfun_poisson_CPU.dat','statfun_poisson_gpu1.dat','s
 	plt.xscale('log',basex=4)
 	#plt.yscale('log',basey=2)
 	ax = plt.gca().xaxis
-	ax.set_major_formatter(ScalarFormatter())
+	if "gpu3" not in files:
+		ax.set_major_formatter(ScalarFormatter())
 	#ax = plt.gca().yaxis
 	#ax.set_major_formatter(ScalarFormatter())
 	plt.savefig('WallTime_%s.png' % names, bbox_inches='tight')
@@ -50,7 +50,7 @@ for files, names in zip(['statfun_poisson_CPU.dat','statfun_poisson_gpu1.dat','s
 	df1 = df1.loc[df1['Size'] >= 100]
 	plt.figure()
 	df1.set_index("Memory", inplace=True)
-	ax = df1.groupby("Size")["Bandwidth"].plot(legend=True, style ='*-')
+	ax = df1["Bandwidth"].plot(legend=True, style ='*-')
 	plt.legend(loc='upper left')
 	plt.xlabel('Memory footprint (Kbytes)')
 	plt.ylabel('Bandwidth (Mflops/s)')
@@ -58,33 +58,51 @@ for files, names in zip(['statfun_poisson_CPU.dat','statfun_poisson_gpu1.dat','s
 	plt.xscale('log',basex=2)
 	plt.yscale('log',basey=2)
 	plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-	ax = plt.gca().xaxis 
-	ax.set_major_formatter(ScalarFormatter()) 
+	ax = plt.gca().xaxis
+	if "gpu3" not in files: 
+		ax.set_major_formatter(ScalarFormatter()) 
 	ax = plt.gca().yaxis 
 	#ax.set_major_formatter(ScalarFormatter()) 
 	plt.savefig('Bandwidth_%s.png' % names, bbox_inches='tight')
 	plt.close()
 
-	df1 = pd.read_csv(files,delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
-	df1 = df1.loc[df1['Size'] >= 100]
-	plt.figure()
-	df1 = df1.sort_values(['Size','Threads'],ascending=True)
-	df_temp = df1.groupby('Size').first().reset_index()[['Size','WallTime']]
-	df_temp = df_temp.rename({'WallTime':'WallTimeFirst'},axis=1)
-	df1 = df1.merge(df_temp,on=['Size'])
-	df1['Speedup'] = df1['WallTimeFirst']/df1['WallTime']
-	df1.set_index("Threads", inplace=True)
-	ax = df1.groupby("Size")["Speedup"].plot(legend=True, style ='*-')
-	plt.plot(np.array([1,2,4,8,16]),np.array([1,2,4,8,16]),'k-',label='Linear scaling')
-	plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-	plt.xlabel('Number of threads')
-	plt.ylabel('Speedup')
-	plt.xscale('log',basex=2)
-	plt.yscale('log',basey=2)
-	ax = plt.gca().xaxis
-	ax.set_major_formatter(ScalarFormatter())
-	ax = plt.gca().yaxis
-	ax.set_major_formatter(FormatStrFormatter('%.2f'))
-	plt.savefig('Speedup_%s.png' % names, bbox_inches='tight')
-	plt.close()
+	
+df1 = pd.read_csv("statfun_poisson_CPU.dat",delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
+df1 = df1.loc[(df1['Size'] >= 100) & (df1['Size'] < 2900)]
+df2 = pd.read_csv("statfun_poisson_gpu1.dat",delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
+df2 = df2.loc[(df2['Size'] >= 100) & (df2['Size'] < 2900)]
+df3 = pd.read_csv("statfun_poisson_gpu2.dat",delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
+df3 = df3.loc[(df3['Size'] >= 100) & (df3['Size'] < 2900)]
+df4 = pd.read_csv("statfun_poisson_gpu3.dat",delim_whitespace=True,header=None,names=["MaxIter","Memory","MFlops","WallTime","Bandwidth","Size"])
+df4 = df4.loc[(df4['Size'] >= 100) & (df4['Size'] < 2900)]
 
+plt.figure()
+df1 = df1.sort_values(['Size'],ascending=True)
+df2 = df2.sort_values(['Size'],ascending=True)
+df3 = df3.sort_values(['Size'],ascending=True)
+df4 = df4.sort_values(['Size'],ascending=True)
+
+#df2['CPU/GPU1'] = df1['WallTime']/df2['WallTime']
+df3['CPU/GPU2'] = df1['WallTime']/df3['WallTime']
+df4['CPU/GPU3'] = df1['WallTime']/df4['WallTime']
+
+df2.set_index("Memory", inplace=True)
+df3.set_index("Memory", inplace=True)
+df4.set_index("Memory", inplace=True)
+
+#ax = df2["Speedup"].plot(legend=True, style ='*-')
+ax = df3["CPU/GPU2"].plot(legend=True, style ='*-')
+ax = df4["CPU/GPU3"].plot(legend=True, style ='*-')
+
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.xlabel('Memory footprint (kbytes)')
+plt.ylabel('Speedup')
+plt.xscale('log',basex=2)
+plt.yscale('log',basey=2)
+ax = plt.gca().xaxis
+#ax.set_major_formatter(ScalarFormatter())
+ax = plt.gca().yaxis
+ax.set_major_formatter(FormatStrFormatter('%.2f'))
+plt.savefig('Speedup.png', bbox_inches='tight')
+plt.close()
+	
